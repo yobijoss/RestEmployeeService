d07d33ef1ca4dfcdb94fe25b339e755048b90cf5
Jose Angel Garcia <jagspage2012@gmail.com>
2019-02-18 09:11:00 -0600

    This is a Spring Boot project that uses the next tech stack: - Java 8 - MySql - OAuth 2

---
+++ src/main/java/com/yobijoss/RestEmployeeService/RestEmployeeServiceApplication.java
@@ -0,0 +1,13 @@
+package com.yobijoss.RestEmployeeService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestEmployeeServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestEmployeeServiceApplication.class, args);
+	}
+
+}
