1c30b2daa96f93bfc627ab38ce1a7c0c58fb998e
José Ángel García Salinas <agarcia@luxoft.com>
2019-02-21 13:23:38 -0600

    Configure Base Auth Security with user and password, also added swagger 2 ui

---
+++ RestEmployeeClient/src/main/java/com/yobijoss/RestEmployeeService/configuration/AppSecurityConfiguration.java
@@ -0,0 +1,55 @@
+package com.yobijoss.RestEmployeeService.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Configuration
+@EnableWebSecurity
+public class AppSecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+
+    private static final String USERNAME = "yobiuser";
+    private static final String PASSWORD = "yobipassword";
+
+    private static final String[] AUTH_WHITELIST = {
+            "/",
+            // -- swagger ui
+            "/v2/api-docs",
+            "/swagger-resources",
+            "/swagger-resources/**",
+            "/configuration/ui",
+            "/configuration/security",
+            "/swagger-ui.html",
+            "/webjars/**"
+    };
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .authorizeRequests()
+                .antMatchers(AUTH_WHITELIST)
+                .permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .httpBasic();
+    }
+
+    @Bean
+    @Override
+    public UserDetailsService userDetailsService() {
+        List<UserDetails> users = new ArrayList<>();
+        users.add(User.withDefaultPasswordEncoder().username(USERNAME).password(PASSWORD).roles("USER", "ADMIN").build());
+        return new InMemoryUserDetailsManager(users);
+    }
+}
